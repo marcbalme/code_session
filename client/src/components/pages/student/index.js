93be3befd33d9be745c3c1620c7cae3e3f8f2bd4
marc <marcbalme9291@gmail.com>
2020-06-30 00:34:12 +0200

    :card_file_box: creatte axios get sutdent

--- client/src/components/pages/student/index.js
+++ client/src/components/pages/student/index.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import Block from '../../block';
 import Button from '../../button';
 import { student } from '../../../translations';
@@ -6,9 +6,20 @@ import Icon from '../../../utils/Icon';
 import { ICON_ADD_USER } from '../../../constants';
 import SlidePanel from '../../slidePanel';
 import CreateStudent from '../../form/CreateStudent';
+import { api_getAllStudent } from '../../../api/student/request';
+import { BDD_URL } from '../../../constants';
+import axios from 'axios';
 
 const StudentPage = () => {
   const [panelIsOpen, setPanelIsOpen] = useState(false);
+  const [students, setStudents] = useState('');
+  useEffect(() => {
+    async function fetchData() {
+      const results = await axios.get(`${BDD_URL}/student`);
+      setStudents(results.data);
+    }
+    fetchData();
+  }, []);
   return (
     <>
       <h1>{student.title}</h1>
